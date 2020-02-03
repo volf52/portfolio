82e8c00668479a4fb6ecddb391bcdadcfb08a861
Arslan <rslnkrmt2552@gmail.com>
2020-02-03 13:00:32 +0300

    Updates

---
+++ precache-manifest.ff40cf8ca24cbc22bdd9bf1b9ab8379c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3148f115b1931d6192276be95f56c4cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "989bc1cd9cae5afe8c11",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5a5dd13fb5e1f5eeb377",
+    "url": "/static/js/2.f175dcd4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.f175dcd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "989bc1cd9cae5afe8c11",
+    "url": "/static/js/main.5e782d4f.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  }
+]);
